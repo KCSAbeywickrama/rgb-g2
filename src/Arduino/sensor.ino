284b615de0e3549945890332ce5ff832888aee17
CSA <abeywickramakcs.19@uom.lk>
2021-04-21 21:54:19 +0530

    readAvgSensor

--- src/Arduino/sensor.ino
+++ src/Arduino/sensor.ino
@@ -8,20 +8,65 @@ void initSensor()
         pinMode(pin, OUTPUT);
 }
 
+unsigned int analogAvgRead(byte pin, byte samples)
+{
+    unsigned int X = 0;
+    unsigned long Y = 0;
+    for (int i = 0; i < samples; i++)
+    {
+        delay(5);
+        unsigned int x = analogRead(pin);
+        X += x;
+        Y += pow(x, 2);
+    }
+    unsigned int mean = X / samples;
+    unsigned long s = sqrt(Y / samples - pow(mean, 2));
+    Serial.print(s);
+    Serial.print('|');
+    return mean;
+}
+
 void readSensor(int *readings)
 {
     for (int i = 0; i < 3; i++)
     {
         digitalWrite(sensorLeds[i], 1);
         delay(200);
-        readings[i] = analogRead(sensorLdr);
+        readings[i] = analogAvgRead(sensorLdr, 30);
         digitalWrite(sensorLeds[i], 0);
+    }
+}
+
+void readAvgSensor(int *readings,byte samples)
+{
+    readings[0] = 0;
+    readings[1] = 0;
+    readings[2] = 0;
+    for (int i = 0; i < samples; i++)
+    {
+        readSensor(readings);
+    }
+    for (int i = 0; i < 3; i++)
+    {
+        readings[i] /= samples;
     }
 }
 
 void showReadings()
 {
+    readAvgSensor(readings,3);
+    Serial.println();
+    for (int reading : readings)
+    {
+        Serial.print(reading);
+        Serial.print(',');
+    }
+    Serial.println();
+    readings[0] = 0;
+    readings[1] = 0;
+    readings[2] = 0;
     readSensor(readings);
+    Serial.println();
     for (int reading : readings)
     {
         Serial.print(reading);
