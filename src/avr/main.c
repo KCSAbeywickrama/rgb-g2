a348e45ce21004efa89e54205df929e56f997850
ashend99 <adikariamad.19@uom.lk>
2021-05-10 22:59:54 +0530

    initialize AVR project

---
+++ src/avr/main.c
@@ -0,0 +1,18 @@
+/*
+ * avr.c
+ *
+ * Created: 07-May-21 5:12:49 PM
+ * Author : CSA
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
