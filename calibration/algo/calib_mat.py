9c1b0bb4d9eba24b59c06082776e9b2ecc203cb8
CSA <abeywickramakcs.19@uom.lk>
2021-06-01 00:46:15 +0530

    calibartion test

--- calibration/algo/calib_mat.py
+++ calibration/algo/calib_mat.py
@@ -1,17 +1,17 @@
 import numpy as np
 
-with open("calibration\data\calibdata1.txt") as fl:
+with open("calibration\data\stage2\calibset2.txt") as fl:
     calib_data=fl.read().split('\n\n')
 
 R,G,B,K,W=calib_data
-sel_calib_data=R,K,W
+sel_calib_data=R,G,B,W
 ex=np.array([tuple(map(int,line.split('\n')[0].split(','))) for line in sel_calib_data])
 rd=np.array([tuple(map(int,line.split('\n')[1].split(','))) for line in sel_calib_data])
 
 
-# print(ex)
-# print()
-# print(rd)
+print(ex)
+print()
+print(rd)
 
 consts=[np.matmul(np.linalg.inv(rd),arr) for arr in np.split(ex,3,1)]
 
