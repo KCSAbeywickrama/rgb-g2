8d805fca1043331f38b1fef32b89cbaafb75fbcb
CSA <abeywickramakcs.19@uom.lk>
2021-06-01 20:25:29 +0530

    add calib core files

---
+++ src/avr/cores/calib.c
@@ -0,0 +1,56 @@
+/*
+* calib.c
+*
+* Created: 01-Jun-21 6:45:02 PM
+*  Author: CSA
+*/
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include "lib/usart.h"
+#include "calib.h"
+
+#define RED 0
+#define GREEN 1
+#define BLUE 2
+
+uint16_t expected[5][3]={
+	{255,0,0},
+	{0,255,0},
+	{0,0,255},
+	{0,0,0},
+	{255,255,255}
+};
+
+uint16_t readings[5][3];
+
+void lcd_init(){
+	uart_init(BAUD_CALC(9600)); // 8n1 transmission is set as default
+	
+	sei(); // enable interrupts, library wouldn't work without this
+}
+
+void lcd_string(char *str){
+	uart_puts(str);	
+}
+
+void lcd_int(uint8_t i){
+	uart_putint(i);
+}
+
+void lcd_char(char ch){
+	uart_putc(ch);
+}
+
+void lcd_int_arr(uint8_t *arr){
+	lcd_int(arr[0]);lcd_char(',');
+	lcd_int(arr[1]);lcd_char(',');
+	lcd_int(arr[2]);	
+}
+
+void get_readings(){
+	lcd_init();
+	lcd_init(5);
+	lcd_char('a');
+	lcd_string("hello");
+}
---
+++ src/avr/cores/calib.h
@@ -0,0 +1,15 @@
+/*
+ * calib.h
+ *
+ * Created: 01-Jun-21 6:45:15 PM
+ *  Author: CSA
+ */ 
+
+
+#ifndef CALIB_H_
+#define CALIB_H_
+
+void get_readings();
+
+
+#endif /* CALIB_H_ */
