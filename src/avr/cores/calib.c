250bf4258f02c0ec4679383d87dccf880129bfec
CSA <abeywickramakcs.19@uom.lk>
2021-06-05 18:20:03 +0530

    implement calibrating functions

--- src/avr/cores/calib.c
+++ src/avr/cores/calib.c
@@ -113,3 +113,108 @@ void println(){
 	uart_puts("\r\n");
 }
 
+void calib_init(){
+	print_init();
+	//while(1){
+		//print_int(sensor_read(5));
+		//println();
+		//_delay_ms(500);
+	//}
+	
+}
+
+void blink(){
+	for(uint8_t i=0;i<5;i++){
+		print_char('.');
+		_delay_ms(1000);
+	}
+	print_char('*');
+	println();
+}
+
+uint16_t max(uint16_t v1,uint16_t v2){
+	if(v1>v2) return v1;
+	return v2;
+}
+
+uint8_t trim(float v){
+	if(v<0) return 0;
+	if(v>255) return 255;
+	return (uint8_t)v;
+}
+
+void calib_start(){
+	
+	print_string("Calibration started\r\n");
+	
+	for(uint8_t i=0;i<3;i++){
+		print_string("Place on ");
+		print_string(colors[i]);
+		blink();
+		get_reading(readings[i]);
+	}
+	
+	for(uint8_t i=0;i<3;i++){
+		print_uint16_arr(readings[i]);
+		println();
+	}
+	println();
+	
+	
+	for(uint8_t i=0;i<3;i++){
+		uint16_t h=readings[i][i];
+		uint16_t l=max(readings[(i+1)%3][i],readings[(i+2)%3][i]);
+		
+		float m=255.0/(h-l);
+		float c=-m*l;
+		
+		coffs[i][0]=m;
+		coffs[i][1]=c;
+		
+		print_float(m);
+		print_char(',');
+		print_float(c);
+		println();
+	}
+	
+	println();
+	
+	print_string("Calibration finished\r\n");
+}
+
+void calc(uint16_t *reading,uint8_t *color){
+	
+	for(uint8_t i=0;i<3;i++){
+		uint8_t value=trim(coffs[i][0]*reading[i]+coffs[i][1]);
+		color[i]=value;
+	}
+}
+
+void get_color(){
+	//for(uint8_t i=0;i<36;i++){
+	//uint8_t calc_color[3];
+	//calc(rd_i[i],calc_color);
+	//print_uint8_arr(calc_color);
+	//println();
+	//}
+	
+	//for(uint8_t i=0;i<5;i++){
+		//uint8_t calc_color[3];
+		//calc(readings[i],calc_color);
+		//print_uint8_arr(calc_color);
+		//println();
+	//}
+	print_string("\r\nPlace on a color");
+	
+	blink();
+	
+	uint16_t reading[3];
+	get_reading(reading);
+	
+	uint8_t calc_color[3];
+	calc(reading,calc_color);
+	print_uint8_arr(calc_color);
+	print_string(" \r\n");
+	_delay_ms(500);
+	
+}
