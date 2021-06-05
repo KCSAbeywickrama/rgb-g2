aafd49f565cf80b7c2d011a458d45e13b06033fa
Lakdilu Abeysinghe <abeysinghealr.19@uom.lk>
2021-06-06 02:51:36 +0530

    sensor.c - calib function added

--- src/avr/cores/sensor.c
+++ src/avr/cores/sensor.c
@@ -28,3 +28,30 @@ uint16_t analog_read (uint8_t _pin){
 	return (ADC); // return ADC value
 }
 
+int calib(uint8_t val, uint8_t clr){
+	const float R_MAX = 400;
+	const float R_MIN = 150;
+	const float G_MAX = 365;
+	const float G_MIN = 98;
+	const float B_MAX = 517;
+	const float B_MIN = 175;
+	
+	if (clr == 0){
+		val = limit(val,R_MAX,R_MIN);
+		val = (val - R_MIN)*(255/(R_MAX - R_MIN));
+	}
+	
+	else if (clr == 1){
+		val = limit(val,G_MAX,G_MIN);
+		val = (val - G_MIN)*(255/(G_MAX - G_MIN));
+	}
+	
+	else if (clr == 2){
+		val = limit(val,B_MAX,B_MIN);
+		val = (val - B_MIN)*(255/(B_MAX - B_MIN));
+	}
+	
+	return val;
+	
+}
+
