dac826ce4fbd4684f6adf665d0d51c1d046433fd
YoltyA <abeysinghewamsy.19@uom.lk>
2021-06-05 21:32:51 +0530

    lcd command function

--- src/avr/cores/lcd.c
+++ src/avr/cores/lcd.c
@@ -17,5 +17,25 @@
 #define RS PB0				/* Define Register Select pin */
 #define EN PD7				/* Define Enable signal pin */
 
+void lcd_command( unsigned char cmnd )
+{
+	//LCD_Port = (LCD_Port & 0x0F) | (cmnd & 0xF0); /* sending upper nibble */
+	LCD_Port = (LCD_Port & 0b1001) | (cmnd & 0b110000)>>3 | (cmnd & 0b11000000)>>2;
+	LCD_Port &= ~ (1<<RS);		/* RS=0*/
+	LCD_EN |= (1<<EN);
+	_delay_us(1);
+	LCD_EN &= ~ (1<<EN);
+
+	_delay_us(200);
+
+	//LCD_Port = (LCD_Port & 0x0F) | (cmnd << 4);  /* sending lower nibble */
+	cmnd<<=4;
+	LCD_Port = (LCD_Port & 0b1001) | (cmnd & 0b110000)>>3 | (cmnd & 0b11000000)>>2;
+	LCD_EN |= (1<<EN);
+	_delay_us(1);
+	LCD_EN &= ~ (1<<EN);
+	_delay_ms(2);
+}
+
 
 
