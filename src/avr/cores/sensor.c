f2391b0cd17e08a34d57f7d5c62a316e31d12515
Lakdilu Abeysinghe <abeysinghealr.19@uom.lk>
2021-06-06 02:50:25 +0530

    sensor.c - analog_read function

--- src/avr/cores/sensor.c
+++ src/avr/cores/sensor.c
@@ -16,5 +16,15 @@ void adc_init(){
 	ADCSRA |= (1<<ADEN) | (1<<ADPS0) | (1<<ADPS1) | (1<<ADPS2); 
 	// Eneble ADC & set divition factor to 128(111) ----> F_ADC = F_CPU/d = 125000 (when F_CPU is 16M)
 }
-
+uint16_t analog_read (uint8_t _pin){
+	_pin &= 0b111; // convert pin to binary
+	
+	ADMUX |= _pin; // Initialize pin which is going to ADC
+	
+	ADCSRA |= (1<<ADSC); // begin the ADC
+	
+	while(ADCSRA & (1<<ADSC)); // delay the function till ADC complete
+	
+	return (ADC); // return ADC value
+}
 
