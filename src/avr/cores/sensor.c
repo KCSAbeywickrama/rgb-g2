cb323ffb75fc79c833130b5fe1313a057c12dece
Lakdilu Abeysinghe <abeysinghealr.19@uom.lk>
2021-08-12 05:15:25 +0530

    AVR - Modified sensor core - add ADIE and DIDR0 registry

--- src/avr/cores/sensor.c
+++ src/avr/cores/sensor.c
@@ -32,13 +32,16 @@ void sensor_init()
 	// REFS = 2 (10) ---> Reserved
 	// REFS = 3 (11) ---> Internal 2.56V with external capasitor in AREF pin
 
-	ADCSRA |= (1 << ADEN) | (1 << ADPS0) | (1 << ADPS1) | (1 << ADPS2);
+	ADCSRA |= (1 << ADEN) | (1 << ADPS0) | (1 << ADPS1) | (1 << ADPS2) | (1 << ADIE);
 	// ADEN ---> Enable ADC
 	// ADC clock frequency must be in a range of 50kHz - 200kHz
 	// ADC result is more accurate in lower frequencies.
 	// Eneble ADC & set divition factor to 128(111) ----> F_ADC = F_CPU/d
 	// For 16MHz microchip best accurate frequency is 125kHz (when devition factor is 128 [Max])
 	// For 8MHz microchip best accurate frequency is 62.5kH (When devition factor is 128) this ADC result is more accuracy than 16MHz microchip.
+	// ADIE - enable interrupt
+	
+	DIDR0 = (1 << LDR_PIN); // Digital input buffer is disabled
 }
 uint16_t _adc_read(uint8_t pin)
 {
